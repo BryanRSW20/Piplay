d7c5028876ff247fd26cbcded4ad0ec5d1125be9
BryanRSW20 <bryanrsw20@outlook.com>
2022-07-01 09:50:21 -0300

    .

--- app/src/main/java/br/com/piplay/MainActivity.java
+++ app/src/main/java/br/com/piplay/MainActivity.java
@@ -1,16 +1,35 @@
 package br.com.piplay;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    Button switchToHomeActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchToHomeActivity = findViewById(R.id.trocaTela);
+        switchToHomeActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivities();
+            }
+        });
     }
+
+    private void switchActivities() {
+        Intent switchActivityIntent = new Intent(this, HomeActivity.class);
+        startActivity(switchActivityIntent);
+    }
+
 }
