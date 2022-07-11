c0e5ee389b4d381d3fe58272c0db652e9eab0478
BryanRSW20 <87527079+BryanRSW20@users.noreply.github.com>
2022-07-11 18:36:07 -0300

    .

--- app/src/main/java/br/com/piplay/EventoActivity.java
+++ app/src/main/java/br/com/piplay/EventoActivity.java
@@ -5,6 +5,8 @@ package br.com.piplay;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +14,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class EventoActivity extends AppCompatActivity {
+
+    ImageView expandDetails;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.evento_activity);
 
+        expandDetails = (ImageView) findViewById(R.id.expandir);
+        expandDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // Bottom Nav e Configuração dos links
         BottomNavigationView bottomNavigationView = findViewById(R.id.BottomNav);
 
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.BottomNav);
---
+++ app/src/main/java/br/com/piplay/EventoExpandActivity.java
@@ -0,0 +1,13 @@
+package br.com.piplay;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EventoExpandActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.evento_details_activity);
+    }
+}
